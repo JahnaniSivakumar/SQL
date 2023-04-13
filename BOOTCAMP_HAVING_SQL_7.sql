62414aa72bb4906becd18caa3521b133a9522c52
JahnaniSivakumar <121501712+JahnaniSivakumar@users.noreply.github.com>
2023-04-13 14:51:56 +0530

    Add files via upload

---
+++ BOOTCAMP_HAVING_SQL_7.sql
@@ -0,0 +1,14 @@
+-- HAVING CLAUSE
+
+SELECT JobTitle, COUNT(JobTitle) FROM SQLTutorial.dbo.EmployeeDemographics
+INNER JOIN SQLTutorial.dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING COUNT(JobTitle) > 1 ;
+
+SELECT JobTitle, AVG(Salary) FROM SQLTutorial.dbo.EmployeeDemographics
+INNER JOIN SQLTutorial.dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING AVG(Salary) > 45000
+ORDER BY AVG(Salary);
